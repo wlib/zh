6e35e23daed08226378e7661b33b13556b8fb0cb
Daniel E <danielethridge@icloud.com>
2018-12-02 02:48:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ const openArticle = async url => {
   document.body.innerHTML = "";
   history.pushState({},"");
   const article = await getArticle(url);
+  article.removeAttribute("property");
+  article.setAttribute("class", "article");
   document.body.appendChild(article);
   const comments = await getComments(url);
   const commentDiv = document.createElement("div");
+  commentDiv.classList.add("comments");
   document.body.appendChild(commentDiv);
   comments.map(comment => {
     const el = document.createElement("div");
+    el.classList.add("comment");
     el.innerHTML = comment.richTextBody;
     commentDiv.appendChild(el);
   })
