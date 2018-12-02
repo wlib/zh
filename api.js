6e749ee69d4545106226b0e5956dd822a6924e69
Daniel E <danielethridge@icloud.com>
2018-12-02 02:08:01 -0500

    Update api.js

--- api.js
+++ api.js
@@ -26,34 +26,6 @@ const getArticle = async url => {
   return article;
 };
 
-const getHTML = async url => {
-  const response = await fetch("https://cors-anywhere.herokuapp.com/" + url);
-  const parser = new DOMParser();
-  return parser.parseFromString(await response.text(), "text/html");
-};
-
-const getCatalog = async (page = 0) => {
-  const d = await getHTML("https://zerohedge.com/?page=" + page);
-  const main = d.body.querySelectorAll("main article.node");
-  return Array.from(main).map(article => ({
-    url: "https://zerohedge.com/" + article.getAttribute("about"),
-    title: article.querySelector(".teaser-title span").innerText,
-    image: "https://zerohedge.com/" + article.querySelector(".teaser-image img").getAttribute("src"),
-    description: article.querySelector(".teaser-text p").innerText
-  }));
-};
-
-const getArticle = async url => {
-  const d = await getHTML(url);
-  const article = d.querySelector("main article .node__content > .text-formatted");
-  const images = article.querySelectorAll("a[data-image-href]");
-  Array.from(images).map(a => {
-    const url = "https://zerohedge.com/" + a.getAttribute("data-image-href");
-    a.parentElement.outerHTML = `<img src="${url}">`
-  });
-  return article;
-};
-
 const getComments = async (url, limit = 100) => {
   const query = `
 query GetComments($url: String!) {
