16b843f59c65446e2203295ba96569da9f1b90fd
Daniel E <danielethridge@icloud.com>
2020-03-03 20:38:33 -0500

    Update api.js

--- api.js
+++ api.js
@@ -1,30 +1,36 @@
-const getHTML = async url => {
-  const response = await fetch("https://cors-anywhere.herokuapp.com/" + url);
-  const parser = new DOMParser();
-  return parser.parseFromString(await response.text(), "text/html");
-};
+const getHtmlDocument = async url => {
+  const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+  const parser = new DOMParser()
+  return parser.parseFromString(await response.text(), "text/html")
+}
 
-const getCatalog = async (page = 0) => {
-  const d = await getHTML("https://www.zerohedge.com/?page=" + page);
-  const main = d.body.querySelectorAll("main article.node");
-  return Array.from(main).map(article => ({
-    url: "https://www.zerohedge.com" + article.getAttribute("about"),
-    title: article.querySelector(".teaser-title span").innerText,
-    image: "https://www.zerohedge.com" + article.querySelector(".teaser-image img").getAttribute("src"),
-    description: article.querySelector(".teaser-text p").innerText
-  }));
-};
+const getArticlePreviews = async (page = 0) => {
+  const pageDOM = await getHtmlDocument(`https://www.zerohedge.com/?page=${page}`)
+  const articles = pageDOM.body.querySelectorAll("main article.node--type-article")
+  return Array.from(articles).map(article => {
+    const header = article.querySelector(".teaser-title")
+    const teaser = article.querySelector(".teaser-content")
+    const image = teaser.querySelector("img")
+    return {
+      url: header.querySelector("a").href,
+      title: header.innerText,
+      image: image.currentSrc,
+      description: teaser.innerText
+    }
+  })
+}
 
 const getArticle = async url => {
-  const d = await getHTML(url);
-  const article = d.querySelector("main article .node__content > .text-formatted");
-  const images = article.querySelectorAll("a[data-image-href]");
+  const pageDOM = await getHtmlDocument(url)
+  const article = pageDOM.querySelector("main article .node__content > .text-formatted")
+  const ads = article.querySelectorAll(".ad__wrapper-element")
+  Array.from(ads).map(a => article.removeChild(a))
+  const images = article.querySelectorAll("a[data-image-href]")
   Array.from(images).map(a => {
-    const url = "https://zerohedge.com/" + a.getAttribute("data-image-href");
-    a.parentElement.outerHTML = `<img src="${url}">`
-  });
-  return article;
-};
+    a.parentElement.outerHTML = `<img src="${a.href}">`
+  })
+  return article
+}
 
 const getComments = async (url, limit = 100) => {
   const query = `
@@ -90,7 +96,7 @@ fragment AnyComment on CommentConnection {
   const response = await fetch("https://cors-anywhere.herokuapp.com/https://talk.zerohedge.com/api/v1/graph/ql", {
     method: "POST",
     headers: {
-     "Content-Type": 'application/json'
+     "Content-Type": "application/json"
     },
     body: JSON.stringify({
       query,
@@ -98,13 +104,13 @@ fragment AnyComment on CommentConnection {
         url
       }
     })
-  });
-  const { data } = await response.json();
-  return data.asset.comments.nodes.sort((a,b) => {
-    const aVotes = a.action_summaries.reduce((acc, cv) => acc + cv.count, 0);
-    const bVotes = b.action_summaries.reduce((acc, cv) => acc + cv.count, 0);
-    if (aVotes < bVotes) return 1;
-    if (aVotes > bVotes) return -1;
-    return 0;
-  });
+  })
+  const { data } = await response.json()
+  return data.asset.comments.nodes.sort((a, b) => {
+    const aVotes = a.action_summaries.reduce((acc, v) => acc + v.count, 0)
+    const bVotes = b.action_summaries.reduce((acc, v) => acc + v.count, 0)
+    if (aVotes < bVotes) return 1
+    if (aVotes > bVotes) return -1
+    return 0
+  })
 }
