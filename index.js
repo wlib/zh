aaf5ca3c4d2d3c9492727d4b01447b6d57ce22e5
Daniel E <danielethridge@icloud.com>
2020-03-03 20:38:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const openCatalog = async page => {
+const showArticlePreviews = async (page = 0) => {
   document.body.innerHTML = "";
-  const catalog = await getCatalog(page);
-  catalog.map(article => {
-    const el = document.createElement("div");
-    const { title, description, image, url } = article;
-    el.innerHTML = `<h1>${title}</h1><p>${description}</p><img src="${image}">`;
-    el.onclick = () => openArticle(url);
-    document.body.appendChild(el);
-  });
-}
-
-const openArticle = async url => {
-  document.body.innerHTML = "";
-  history.pushState({},"");
-  const article = await getArticle(url);
-  article.removeAttribute("property");
-  article.setAttribute("class", "article");
-  document.body.appendChild(article);
-  const comments = await getComments(url);
-  const commentDiv = document.createElement("div");
-  commentDiv.classList.add("comments");
-  document.body.appendChild(commentDiv);
-  comments.map(comment => {
-    const el = document.createElement("div");
-    el.classList.add("comment");
-    el.innerHTML = comment.richTextBody;
-    commentDiv.appendChild(el);
+  const articles = await getArticlePreviews(page);
+  articles.map(article => {
+    const { title, description, image, url } = article
+    const e = document.createElement("article")
+    e.innerHTML = `<h1>${title}</h1> <p>${description}</p> <img src="${image}">`
+    e.onclick = () => showArticle(url)
+    document.body.appendChild(e)
   })
 }
 
-addEventListener("popstate", () => openCatalog());
-openCatalog();
+const showArticle = async url => {
+  document.body.innerHTML = ""
+  history.pushState({}, "")
+  const article = await getArticle(url)
+  article.removeAttribute("property")
+  article.setAttribute("class", "article")
+  document.body.appendChild(article)
+  const comments = await getComments(url)
+  const commentDiv = document.createElement("div")
+  commentDiv.classList.add("comments")
+  document.body.appendChild(commentDiv)
+  comments.map(comment => {
+    const el = document.createElement("div")
+    el.classList.add("comment")
+    el.innerHTML = comment.richTextBody
+    commentDiv.appendChild(el)
+  })
+}
+
+addEventListener("popstate", () => showArticlePreviews())
+showArticlePreviews()
